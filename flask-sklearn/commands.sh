08eb3635dc443c8a9c7036af32aa0d3dca0be7cc
Lauren Nicholls <lnicholls@relias.com>
2023-07-12 10:28:22 -0400

    update commands.sh file

--- flask-sklearn/commands.sh
+++ flask-sklearn/commands.sh
@@ -1,2 +1,9 @@
+name="nichollsflaskapp"
+resourceGroup="Azuredevops"
+runtime="PYTHON|3.7"
+
 # spin up azure application
-az webapp up --name nichollsflaskapp --runtime "PYTHON|3.7" --resource-group Azuredevops
+az webapp up --name $name --runtime $runtime --resource-group $resourceGroup
+
+# show url
+az webapp show --name $name --resource-group $resourceGroup --query defaultHostName --output tsv
