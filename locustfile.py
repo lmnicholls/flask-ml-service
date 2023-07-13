80b10163b6c23173c9a1f1a6ff18ed91d3641915
Lauren Nicholls <lnicholls@relias.com>
2023-07-13 10:51:25 -0400

    add locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -6,4 +6,23 @@ class MyUser(HttpUser):
     
     @task(1)
     def predicttest(self):
-        self.client.post("http://localhost:5000/predict")
+        self.client.post("http://localhost:5000/predict", {
+            "CHAS":{
+            "0":0
+            },
+            "RM":{
+            "0":6.575
+            },
+            "TAX":{
+            "0":296.0
+            },
+            "PTRATIO":{
+            "0":15.3
+            },
+            "B":{
+            "0":396.9
+            },
+            "LSTAT":{
+            "0":4.98
+            }
+        })
