69e3aa2c1f634e46e02246a88d86dceb76f0a284
Lauren Nicholls <lnicholls@relias.com>
2023-07-13 09:59:01 -0400

    update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def predict():
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
-    # inference_payload = pd.DataFrame(json_payload)
-    # LOG.info("inference payload DataFrame: %s inference_payload")
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info("inference payload DataFrame: %s inference_payload")
     # scaled_payload = scale(inference_payload)
     # prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': "prediction"})
