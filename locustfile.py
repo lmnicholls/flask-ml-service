a847076d593e5f47b4a70c746ddf813c582348f5
Lauren Nicholls <lnicholls@relias.com>
2023-07-13 09:17:13 -0400

    add locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,30 @@
+from locust import HttpUser, task, between
+
+class MyUser(HttpUser):
+    # Define wait time between tasks
+    wait_time = between(1, 5)
+    host = "https://nichollsflaskapp.azurewebsites.net"
+    
+    @task
+    def predict(self):
+        payload = {
+            "CHAS":{
+                "0":0
+            },
+            "RM":{
+                "0":6.575
+            },
+            "TAX":{
+                "0":296.0
+            },
+            "PTRATIO":{
+                "0":15.3
+            },
+            "B":{
+                "0":396.9
+            },
+            "LSTAT":{
+                "0":4.98
+            }
+        }
+        self.client.post("/predict", payload)
