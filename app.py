2159de2d8c66ea305e8f982e87c535143074e79f
Lauren Nicholls <lnicholls@relias.com>
2023-07-13 10:25:02 -0400

    update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home - Nicholls Flask App</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
@@ -43,10 +43,6 @@ def predict():
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': prediction})
-
-@app.route("/test", methods=['POST'])
-def test():
-    return jsonify({'prediction': "prediction"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
