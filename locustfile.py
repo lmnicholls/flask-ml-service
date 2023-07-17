af3994a7085918170261aee4d37abe44fba9e461
Lauren Nicholls <lnicholls@relias.com>
2023-07-17 13:51:21 +0000

    update locust test file

--- locustfile.py
+++ locustfile.py
@@ -37,4 +37,4 @@ class MyUser(HttpUser):
             }
         }
         
-        self.client.post("http://localhost:5000/predict", data=payload, headers=headers)
+        self.client.post("http://localhost:5000/predict", headers=headers, json=payload)
